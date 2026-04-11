ab8ec8f9d0e1620fc7f3d5a3f4eb24df2d36f0ea
Prudy8 <DeborahMcDougall10@gmail.com>
2026-04-10 21:40:29 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 from app.routers import main_router
+from app.routers.category import category_router
 
 app = FastAPI()
 app.include_router(main_router)
+app.include_router(category_router)
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
